32815d231e4ce378739848ea9c3e61b2bd7c10c2
blbergo <bryan.l.bergo@gmail.com>
2024-04-08 14:21:08 -0700

    nativewind setup

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="bg-green-500 w-full min-h-screen flex flex-col items-center justify-center">
+      <Text>Hello, world! The background should be green, and this text should be centered.</Text>
+    </View>
+  );
+}
