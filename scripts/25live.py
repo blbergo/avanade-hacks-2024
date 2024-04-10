0cadddd9f40ba966a69a9ec666d76f9c98314af7
blbergo <bryan.l.bergo@gmail.com>
2024-04-09 20:26:28 -0700

    more data

--- scripts/25live.py
+++ scripts/25live.py
@@ -1,6 +1,5 @@
 import wget
 
-url = 'https://25live.collegenet.com/25live/data/cpp/run/space/detail/spdetail.json?space_id='
-
-for i in range(1, 561):
-    wget.download(f"{url}{i}", f"data/space-{i}.json")
+for i in range(1,562):
+    url = f"https://25live.collegenet.com/25live/data/cpp/run/list/listdata.json?compsubject=location&sort=name&order=asc&page={i}&page_size=1&obj_cache_accl=0&category_id=112+2+3+4+5+6+113+114+119+115+120+118+116+117+8+10+15+16+17+18+19+21+22+23+24+25+26+27+28+29+31+32+33+34+35+36+37+38+42+39+40+41+43+45&caller=pro-ListService.getData"
+    wget.download(url, f"data/row-{i}.json")
--- scripts/chroma_store.py
+++ scripts/chroma_store.py
@@ -1,14 +1,32 @@
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings.sentence_transformer import (
     SentenceTransformerEmbeddings,
 )
-from langchain_community.vectorstores import Chroma
-from langchain.document_loaders import DirectoryLoader, TextLoader
+import json
+
 
 loader = DirectoryLoader("data", glob='**/*.json', show_progress=True, loader_cls=TextLoader)
+docs = loader.load()
+data = []
 
+for doc in docs:
+    res = {}
+    content = doc.page_content
+    serialized = json.loads(content)
+    
+    res["name"] = serialized["rows"][0]["row"][1]
+    res["categories"] = serialized["rows"][0]["row"][2]
+    res["features"] = serialized["rows"][0]["row"][3]
+    res["max_capacity"] = serialized["rows"][0]["row"][5]
+    res["capacity"] = serialized["rows"][0]["row"][6]
+    res["building"] = serialized["rows"][0]["row"][7]["itemName"]
+    
+    data.append(res)
+    
+loader = TextLoader(data)
 docs = loader.load()
 
 embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-db = Chroma.from_documents(docs, embedding_function)
 
 db2 = Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db")
